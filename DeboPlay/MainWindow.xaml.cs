10f5e0d836c12557096d4855dde1bb7df4ccf9ec
ZohanHaqu <161150779+ZohanHaqu@users.noreply.github.com>
2025-03-11 17:31:12 -0400

    Add project files.

---
+++ DeboPlay/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace DeboPlay
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            this.MouseLeftButtonDown += new MouseButtonEventHandler(Window_MouseLeftButtonDown);
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                progressBar.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+                progressBar.Value = mediaElement.Position.TotalSeconds;
+            }
+        }
+
+        private void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media Files|*.mp4;*.mkv;*.avi;*.mov;*.wmv;*.flv;*.webm;*.mpg;*.mpeg;*.3gp;*.ogv;*.mts;*.m2ts;*.vob;*.ts;*.divx;*.rmvb;*.mp3;*.aac;*.wav;*.flac;*.alac;*.ogg;*.wma;*.aiff;*.opus;*.dts;*.ac3;*.amr;*.m4a;*.mid;*.midi;*.pcm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaElement.Source = new Uri(openFileDialog.FileName);
+                mediaElement.Play();
+                timer.Start();
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+            timer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+            timer.Stop();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+            timer.Stop();
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                mediaElement.Position = TimeSpan.FromSeconds(progressBar.Value);
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+    }
+}
---
+++ DeboPlay/formats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeboPlay
+{
+    internal class Formats
+    {
+        public static readonly List<string> SupportedFormats = new List<string>
+        {
+            ".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".webm", ".mpg", ".mpeg",
+            ".3gp", ".ogv", ".mts", ".m2ts", ".vob", ".ts", ".divx", ".rmvb", ".mp3",
+            ".aac", ".wav", ".flac", ".alac", ".ogg", ".wma", ".aiff", ".opus", ".dts",
+            ".ac3", ".amr", ".m4a", ".mid", ".midi", ".pcm"
+        };
+    }
+}
